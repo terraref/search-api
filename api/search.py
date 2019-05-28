3af7ff192cc235e2db1f5b677fc7a4d192300738
toddn <todd.nicholson@gmail.com>
2019-05-28 15:30:25 -0500

    stub added for count with

--- api/search.py
+++ api/search.py
@@ -4,4 +4,6 @@ import helper
 
 def search(season=None, experimentId=None, cultivarId=None, treatmendId=None, pageSize=None, page=None):
     # load all the data
-    return 0
+    data = {"leadPersonDbId": "person1", "leadPersonName": "Name Nameson"}
+    count = 1
+    return helper.create_result({"data": data}, count, pageSize, page)
