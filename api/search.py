adeb927e216e2f903390090d09e1b59e8f714060
toddn <todd.nicholson@gmail.com>
2019-05-28 16:07:09 -0500

    search now works, placeholder data

--- api/search.py
+++ api/search.py
@@ -7,3 +7,6 @@ def search(season=None, experimentId=None, cultivarId=None, treatmendId=None, pa
     data = {"leadPersonDbId": "person1", "leadPersonName": "Name Nameson"}
     count = 1
     return helper.create_result({"data": data}, count, pageSize, page)
+
+def get(season=None, experimentId=None, cultivarId=None, treatmendId=None, pageSize=None, page=None):
+    return "this is the page"
