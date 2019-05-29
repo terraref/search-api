1be2203e7ef9e699c10de1292d69efe22c00e7a9
toddn <todd.nicholson@gmail.com>
2019-05-29 16:05:29 -0500

    adding plots, seasons classes

---
+++ api/plots.py
@@ -0,0 +1,9 @@
+import json
+
+data = json.load(open('data/plots.json', 'r'))
+
+def search(season=None, experimentId=None, cultivarId=None, treatmendId=None, pageSize=None, page=None):
+    return data
+
+def get(season=None, experimentId=None, cultivarId=None, treatmendId=None, pageSize=None, page=None):
+    return "this is the page"
