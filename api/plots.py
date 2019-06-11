e3f162f7308b471f22017c2419e8176779f7b26e
toddn <todd.nicholson@gmail.com>
2019-06-11 15:25:08 -0500

    replacing 'cultivar' with 'germplasm' to match brapi specifications

--- api/plots.py
+++ api/plots.py
@@ -2,8 +2,8 @@ import json
 
 data = json.load(open('data/plots.json', 'r'))
 
-def search(season=None, experimentId=None, cultivarId=None, treatmendId=None, pageSize=None, page=None):
+def search(season=None, experimentId=None, germplasmId=None, treatmendId=None, pageSize=None, page=None):
     return data
 
-def get(season=None, experimentId=None, cultivarId=None, treatmendId=None, pageSize=None, page=None):
+def get(season=None, experimentId=None, germplasmId=None, treatmendId=None, pageSize=None, page=None):
     return "this is the page"
