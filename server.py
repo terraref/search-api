fa4cce44d81b5197da56b3d7c1f6dc04521def1a
toddn <todd.nicholson@gmail.com>
2019-06-12 13:41:19 -0500

    CORS does not work

--- server.py
+++ server.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
-import logging
-
+import logging, logging.config
+logging.basicConfig()
+import os
 import connexion
 from searchresolver import SearchResolver
+from flask import Flask
+from flask import Flask, render_template, send_file, request, url_for, redirect, make_response
+from flask import jsonify
 from flask_cors import CORS, cross_origin
 
 
@@ -21,20 +25,19 @@ def basic_auth(username, password, required_scopes=None):
     print("basic_auth")
     return {'user': 'admin'}
 
-
-if __name__ == '__main__':
-    debug = False
-
-    if debug:
-        logging.basicConfig(format='%(asctime)-15s %(message)s', level=logging.DEBUG)
-    else:
-        logging.basicConfig(format='%(asctime)-15s %(message)s', level=logging.INFO)
-
+def create_app():
     app = connexion.FlaskApp(__name__, debug=debug)
 
+
+
+
+
+
+    app.add_api('search.yaml',
+                arguments={'title': 'TERRA advanced search api'},
+                resolver=SearchResolver('api'),
+                resolver_error=501)
     CORS(app)
-
-
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Origin', '*')
@@ -42,10 +45,32 @@ if __name__ == '__main__':
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
         return response
 
-    app.add_api('search.yaml',
-                arguments={'title': 'TERRA advanced search api'},
-                resolver=SearchResolver('api'),
-                resolver_error=501)
+    @app.route('/')
+    def default():
+        return "this is the defaultpage"
 
-    # app.run(port=5000, host=None, server='flask', debug=debug, use_reloader=False)
-    app.run(port=5000, host=None, server='flask', debug=debug)
+
+    return app
+
+def main():
+    apiIP = os.getenv('COUNTER_API_IP', "0.0.0.0")
+    apiPort = os.getenv('COUNTER_API_PORT', "5000")
+    logger.info("*** API now listening on %s:%s ***" % (apiIP, apiPort))
+    search_app = create_app()
+    search_app.run(port=5000, host=None, server='flask', debug=debug)
+
+
+if __name__ == '__main__':
+    debug = False
+    logger = logging.getLogger('search-api')
+    logger.setLevel('INFO')
+    if debug:
+        logging.basicConfig(format='%(asctime)-15s %(message)s', level=logging.DEBUG)
+    else:
+        logging.basicConfig(format='%(asctime)-15s %(message)s', level=logging.INFO)
+
+    main()
+
+
+
+
