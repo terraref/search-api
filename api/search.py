5f40a13be5a85c6ef35082cb74ef8651f9dcd97a
toddn <todd.nicholson@gmail.com>
2019-07-03 10:21:45 -0500

    links for geotiffs

--- api/search.py
+++ api/search.py
@@ -2,8 +2,8 @@ import json
 
 data = json.load(open('dummy-data/searchResults.json', 'r'))
 
-def search(season=None, experimentId=None, germplasmId=None, treatmendId=None, pageSize=None, page=None):
+def search(season=None, experimentId=None, germplasmId=None, treatmendId=None, product=None, pageSize=None, page=None):
     return data
 
-def get(season=None, experimentId=None, germplasmId=None, treatmendId=None, pageSize=None, page=None):
+def get(season=None, experimentId=None, germplasmId=None, treatmendId=None, product=None, pageSize=None, page=None):
     return "this is the page"
