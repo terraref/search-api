b59ef4e4cb4b7075add9778ea603c2c2c175afb3
toddn <todd.nicholson@gmail.com>
2019-07-03 10:35:59 -0500

    adding product to yaml

--- api/search.py
+++ api/search.py
@@ -3,6 +3,11 @@ import json
 data = json.load(open('dummy-data/searchResults.json', 'r'))
 
 def search(season=None, experimentId=None, germplasmId=None, treatmendId=None, product=None, pageSize=None, page=None):
+    if product:
+        if product == 'IR GeoTIFF':
+            return product
+        elif product == 'Canopy Cover':
+            return product
     return data
 
 def get(season=None, experimentId=None, germplasmId=None, treatmendId=None, product=None, pageSize=None, page=None):
