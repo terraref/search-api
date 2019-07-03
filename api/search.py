7b5d057c29628539e0f7fb54911fa9f692414108
toddn <todd.nicholson@gmail.com>
2019-07-03 10:44:20 -0500

    links for geotiff

--- api/search.py
+++ api/search.py
@@ -1,11 +1,12 @@
 import json
 
 data = json.load(open('dummy-data/searchResults.json', 'r'))
+geotiff_data = json.load(open('dummy-data/geotiffSearchResults.json', 'r'))
 
 def search(season=None, experimentId=None, germplasmId=None, treatmendId=None, product=None, pageSize=None, page=None):
     if product:
         if product == 'IR GeoTIFF':
-            return product
+            return geotiff_data
         elif product == 'Canopy Cover':
             return product
     return data
