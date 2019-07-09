ddeb32d49b4918a17a86c5343791fe6852bd46bc
toddn <todd.nicholson@gmail.com>
2019-07-09 16:41:01 -0500

    using bety_helper to get result

--- api/search.py
+++ api/search.py
@@ -1,4 +1,6 @@
 import json
+from helpers import bety_helper
+import os
 
 data = json.load(open('dummy-data/searchResults.json', 'r'))
 geotiff_data = json.load(open('dummy-data/geotiffSearchResults.json', 'r'))
@@ -9,7 +11,8 @@ def search(season=None, experimentId=None, germplasmId=None, treatmendId=None, p
         if product == 'IR GeoTIFF':
             return geotiff_data
         elif product == 'Canopy Cover':
-            return canopycover_data
+            if season:
+                result = bety_helper.get_canopy_cover_sitename(season,bety_key=os.environ['BETY_KEY'])
     return data
 
 def get(season=None, experimentId=None, germplasmId=None, treatmendId=None, product=None, pageSize=None, page=None):
