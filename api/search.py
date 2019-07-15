fb905d373e736e5ebc000c2218e7081e96ca7a2b
toddn <todd.nicholson@gmail.com>
2019-07-15 10:06:53 -0500

    adding error handling

--- api/search.py
+++ api/search.py
@@ -1,9 +1,10 @@
 import json
 from helpers import bety_helper, clowder_helper
 import os
-from flask import Flask, Response
+from flask import Flask, Response, abort
 from flask import Flask, render_template, send_file, request, url_for, redirect, make_response
 from io import StringIO
+from werkzeug.exceptions import BadRequest
 
 data = json.load(open('dummy-data/searchResults.json', 'r'))
 geotiff_data = json.load(open('dummy-data/geotiffSearchResults.json', 'r'))
@@ -28,11 +29,17 @@ def search(season=None, date=None, start_date=None, end_date=None, experimentId=
             elif start_date and end_date:
                 result = clowder_helper.get_clowder_result_date_range(product, start_date, end_date)
                 return {"clowder": result, "bety": []}
+            else:
+                return abort(400, "Need start_date and end_date for product : " + product)
         elif str(product) in bety_products:
             #result = bety_helper.get_trait_sitename('Season ' + season, trait=product, bety_key=os.environ['BETY_KEY'])
             result = bety_helper.get_bety_search_result('Season ' + season, trait=product)
             return {"clowder": [], "bety": [result]}
+        else:
+            return abort(400, "No product matches " + product)
     else:
+        if not (experimentId or germplasmId or treatmendId):
+            return abort(400, "Correct search parameters not provided")
         return {"clowder": [], "bety": []}
 
 def get(season=None, date=None, start_date=None, end_date=None, experimentId=None, germplasmId=None, treatmendId=None, product=None, pageSize=None, page=None):
